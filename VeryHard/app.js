1aedd1722ba3f9d6aed5140a9eaddab3aca83fb0
chrism3lo <christopher.melopelaez@gmail.com>
2022-05-11 19:52:05 -0400

    VeryHard File

--- VeryHard/app.js
+++ VeryHard/app.js
@@ -1,6 +1,31 @@
-let coins = [1, 2, 5];
-let amount = 11;
+let coins = [1, 5, 10, 25];
+let amount = 60;
 
 function coinmaker (coins, amount){
-    
+    let coinsAmnt = 0;
+    let bigggestCoin;
+    let explanation = [];
+
+    while (amount > 0){
+        bigggestCoin = Math.max(...coins)
+        if (bigggestCoin > amount){
+            coins = removeLargest(coins);
+            if (!coins.length) return -1;
+        
+        } else {
+            amount -= bigggestCoin;
+            explanation.push(bigggestCoin);
+            coinsAmnt++;
+        }
+    }
+    console.log(...explanation);
+    return coinsAmnt;
 }
+
+function removeLargest(coins){
+    let bigggestCoin = Math.max(...coins)
+    bigggestCoin = coins.filter(coins => coins !== bigggestCoin)
+    return bigggestCoin;
+}
+
+console.log(coinmaker(coins, amount));
